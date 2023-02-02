5f33e4c185661d97f97c22ed63f521f445fff161
idrisabkar <idrisabkar2018@gmail.com>
2023-02-02 12:14:54 +0300

    twelve_com

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, render_template, jsonify
+from datetime import datetime
+
+from flask import Flask, request, render_template, jsonify, json
 import csv
 
 app = Flask(__name__)
+attendance_data = {}
 
 
 def csv_to_dict(file_path):
@@ -14,8 +17,30 @@ def csv_to_dict(file_path):
 attendance = csv_to_dict("names.csv")
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
+    # Get the client's IP address
+    client_ip = request.remote_addr
+    print(client_ip)
+
+    # Check if the IP address is in the attendance data
+    if client_ip in attendance_data:
+        # Get the last access time for this IP address
+        last_access = attendance_data[client_ip]
+
+        # Calculate the time difference in minutes
+        time_diff = (datetime.now() - last_access).total_seconds() / 60
+
+        # If the time difference is less than 5 minutes, return an error message
+        if time_diff < 5:
+            return json.dumps({"error": "Please wait before accessing the system again"}), 429
+
+        # If the time difference is 5 minutes or more, update the access time
+        attendance_data[client_ip] = datetime.now()
+    else:
+        # If the IP address is not in the attendance data, add it
+        attendance_data[client_ip] = datetime.now()
+
     return render_template('index.html')
 
 
